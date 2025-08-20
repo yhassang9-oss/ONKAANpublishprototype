bff530cc3e314cf3d0075c619da9a02af52b97d6
Yachang Hassang Industry <yhassang9@gmail.com>
2025-08-20 10:34:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -40,4 +40,7 @@ app.post("/publish", (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
